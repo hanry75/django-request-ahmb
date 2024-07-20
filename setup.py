1a9317c11d660e7e75e3ff6cdc99c872015b5320
hanry75 <hanry75@users.noreply.github.com>
2024-07-20 12:40:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Django>=4.2',
+        'Django>=3.8',
     ],
     classifiers=[
         'Framework :: Django',
