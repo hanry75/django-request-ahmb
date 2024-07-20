7d8be02d35ae446b74abe53b3faf00ff85b3a88c
hanry75 <hanry75@users.noreply.github.com>
2024-07-20 11:10:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-request-ahmb',
+    name='django_request_ahmb',
     version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
